f3a3f052de6a6328e63b79fd9e3573238398d652
Artur Trofimov <arturtr@gmail.com>
2012-11-20 00:22:33 +0400

    Fix gitignore

---
+++ config/initializers/insales_api.rb
@@ -0,0 +1,4 @@
+MyApp.api_key            = '4bd1ef4661fbeafe83aebfa52f80ceb1'
+MyApp.api_secret         = '37c98e22da44201bde24bf8c44f3cfb5'
+MyApp.api_host           = 'arturtr.myinsales.ru'
+MyApp.api_autologin_path = 'session/autologin'
