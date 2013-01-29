5de9c325c74f096f00c272beb73de048e839ee10
Artur Trofimov <arturtr@gmail.com>
2013-01-29 14:45:13 +1100

    add shop, token, insales_id to attr accesible

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
-  attr_accessible :last_updated, :updated_categories
+  attr_accessible :last_updated, :updated_categories, :shop, :token, :insales_id
   serialize :updated_categories
   validates_presence_of :insales_id
   validates_presence_of :insales_subdomain
